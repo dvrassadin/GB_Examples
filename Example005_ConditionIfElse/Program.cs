3b33f074c9bc4632695c5868d8d57c500ab6eb8d
Daniil Rassadin <daniil@rassadin.org>
2022-11-28 14:24:07 +0600

    Add Example005 ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите Ваше имя: ");
+string userName = Console.ReadLine();
+if(userName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
