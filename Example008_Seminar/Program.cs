db86fa3165f4b1eae6bb82b4e741cd8a9460797a
Daniil Rassadin <daniil@rassadin.org>
2022-11-28 17:33:12 +0600

    Example008 Seminar

---
+++ Example008_Seminar/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int time = 0;
+bool isDirectionFirstFriend = false;
+
+while (distance > 10)
+{
+    
+    if(isDirectionFirstFriend)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        isDirectionFirstFriend = false;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        isDirectionFirstFriend = true;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+Console.WriteLine($"Собака пробежит {count} раз(а).");
